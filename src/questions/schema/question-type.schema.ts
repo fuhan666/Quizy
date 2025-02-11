c47571f4a358d9b44ca8854658208f271951f335
fuhan <fuhan666@outlook.com>
2025-02-11 08:40:29 +0800

    chore: remove test files

--- src/questions/schema/question-type.schema.ts
+++
@@ -1,7 +0,0 @@
-export enum questionType {
-    SINGLE_CHOICE = 'SINGLE_CHOICE', // 单选题
-    MULTIPLE_CHOICE = 'MULTIPLE_CHOICE', // 多选题
-    TRUE_FALSE = 'TRUE_FALSE', // 判断题
-    FILL_IN_BLANK = 'FILL_IN_BLANK', // 填空题
-
-}

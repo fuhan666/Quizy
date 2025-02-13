4305a682e45f6dc40d25c67318bf8477d7644eb9
fuhan <hanfu.hf@gmail.com>
2025-02-13 14:08:58 +0800

    fix: fixed the issue where users could not be created

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,11 +10,13 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { SkipAuth } from 'src/auth/skip-auth';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @SkipAuth()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
